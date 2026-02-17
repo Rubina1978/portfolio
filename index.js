1c0d9fa97ed429459980d51d1ad9e78f3d06fb06
Rubina1978 <hrubesova@gmail.com>
2026-02-17 23:21:33 +0000

    complete idex.html

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ document.querySelectorAll('.card').forEach(card => {
         }
     });
 });
+
+document.querySelectorAll('.logo').forEach(card => {
+    card.addEventListener('click', function(e) {
+        // Only toggle on non-hover devices (touch)
+        if (!window.matchMedia('(hover: hover)').matches) {
+            card.classList.toggle('active');
+            e.preventDefault();
+        }
+    });
+});
